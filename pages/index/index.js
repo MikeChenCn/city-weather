88dcc85c356a6661c881bb8e64046f1cf2052992
MikeChen <790670714@qq.com>
2019-02-24 22:43:17 +0800

    首页制作与后台数据渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,27 +4,51 @@ const app = getApp();
 const bmap = require('../../libs/bmap-wx.js');
 
 Page({
-  data:{
-    currentWeather:''
+  data: {
+    // currentWeather: '',
+    // originalData:'',
+    // curLifeDate:[]
   },
-  onLoad: function () {
+  onLoad: function() {
     let BMap = new bmap.BMapWX({
       ak: '6BvK0e6fxvzYVPN2TPNQP5Iy7g0RHPpV'
     });
-    let that=this;
+    let that = this;
     BMap.weather({
-      fail: function (data) {
+      fail: function(data) {
         console.log('查询失败')
       },
-      success: function (data) {
+      success: function(data) {
         console.log('查询成功');
-        var currentWeather = data;
+        // console.log(currentWeather);
 
-        
-        console.log(currentWeather);
+        var currentWeather = data.currentWeather[0];
+        var curDate = currentWeather.date.split(' ');
+        var week = curDate[0];
+        var date = curDate[1];
+        var num = curDate[2].replace(/[^0-9]/ig, "");//获取实时温度
+
+        var curCity = currentWeather.currentCity;
+        var originalData = data.originalData.results[0];
+        var curLifeDate = originalData.index;
+        var curData={
+          week:week,
+          date: date,
+          curCity: curCity
+        }
+
+        console.log(curLifeDate);
+        console.log(curDate);
 
         that.setData({
-          currentWeather: currentWeather
+          currentWeather: currentWeather,
+          originalData: originalData,
+          curLifeDate: curLifeDate,
+          week:week,
+          date:date,
+          curCity: curCity,
+          num: num,
+          curData: curData
         });
       }
     });
