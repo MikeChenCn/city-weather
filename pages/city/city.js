db724b59847317112fa7d47340bb5c215bcea296
790670714 <f8d_790670714_jian523@git.cloud.tencent.com>
2019-02-26 15:48:14 +0800

    城市搜索列表页面实现

--- pages/city/city.js
+++ pages/city/city.js
@@ -1,6 +1,7 @@
 // pages/city/city.js
 const app = getApp();
 const bmap = require('../../libs/bmap-wx.js');
+let cityData=require('../../data/cityData.js');
 
 Page({
 
@@ -8,41 +9,27 @@ Page({
      * 页面的初始数据
      */
     data: {
-      sugData: '' 
+      sugData: '' ,
+      // hotCities:[]
     },
     
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    
 
+    let cityList = cityData.cityData.data;
+    let hotCities = cityList.hotCities;
+    let cities = cityList.cities;
+
+    this.setData({
+      hotCities: hotCities,
+      cities: cities
+    })
   },
   bindKeyInput: function (e) {
-    var that = this;
-    // 新建百度地图对象 
-    var BMap = new bmap.BMapWX({
-      ak: '6BvK0e6fxvzYVPN2TPNQP5Iy7g0RHPpV'
-    });
-    var fail = function (data) {
-      console.log(data)
-    };
-    var success = function (data) {
-      var sugData = '';
-      for (var i = 0; i < data.result.length; i++) {
-        sugData = sugData + data.result[i].name + '\n';
-      }
-      that.setData({
-        sugData: sugData
-      });
-    }
-    // 发起suggestion检索请求 
-    BMap.suggestion({
-      query: e.detail.value,
-      region: '北京',
-      city_limit: true,
-      fail: fail,
-      success: success
-    });
+
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
